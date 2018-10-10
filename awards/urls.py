5e12d007d4c56cb5cd7f550e313f08fcda7c0651
IreriVikki <wambsviki@gmail.com>
2018-10-10 23:19:38 +0300

    created a user login template that works

--- awards/urls.py
+++ awards/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 
-urlpatterns=[
-    url('^$',views.home ,name = 'home'),
+urlpatterns = [
+    url('^$', views.home, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^userdetails/$', views.profile, name='profile'),
 ]
--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import HttpResponse, render, redirect, get_object_or_404, reverse, get_list_or_404
+from django.core.mail import mail_admins
+from django.contrib.auth.models import User
+from django.contrib import auth
+import datetime
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            # correct username and password login the user
+            auth.login(request, user)
+            return redirect('user_details')
+
+        # else:
+        #     messages.error(request, 'Error wrong username/password')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'index.html')
+
+
+def profile(request):
+    user = get_object_or_404(User, id=request.user.id)
+    return render(request, 'profile.html', {'user': user})
