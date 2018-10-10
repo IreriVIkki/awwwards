373727b3e8f6b5bd579b13707591468093438b15
IreriVikki <wambsviki@gmail.com>
2018-10-10 22:19:14 +0300

    added a javascript file to the project

---
+++ static/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // hover over landing page
+    $('.landing').hover(function () {
+        // over
+        $('.landing-abs-fade').addClass('dark-fade');
+        $('.landing-abs-fade').removeClass('light-fade');
+    }, function () {
+        // out
+        $('.landing-abs-fade').addClass('light-fade');
+        $('.landing-abs-fade').removeClass('dark-fade');
+    });
+
+    // hover over individual website box
+    $('.site-img').hover(function () {
+        // over
+        $('.site-fade').addClass('site-fade-in');
+        $('.site-fade').removeClass('site-fade-out');
+    }, function () {
+        // out
+        $('.site-fade').removeClass('site-fade-in');
+        $('.site-fade').addClass('site-fade-out');
+    });
+});
