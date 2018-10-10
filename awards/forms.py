79e3abecbc990d6208aed33ef262467d50c82880
IreriVikki <wambsviki@gmail.com>
2018-10-11 00:47:29 +0300

    created a super beautifully custom user registration page....... goddamn it's looks fantastic

--- awards/forms.py
+++ awards/forms.py
@@ -8,7 +8,6 @@ class MyRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
-    birtday = forms.DateField(required=False)
 
     class Meta:
         model = User
