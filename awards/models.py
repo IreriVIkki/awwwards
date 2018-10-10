82f6fc3a7e7a045b1f034e1e1c4e779409687c3e
IreriVikki <wambsviki@gmail.com>
2018-10-11 01:43:26 +0300

    added all the models i think i will need for the project this far

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    pass
+
+class Post(models.Model):
+    pass
+
+class tags(models.Model):
+    pass
+
+class technologies(models.Model):
+    pass
+
+class Rating(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Collection(models.Model):
+    pass
+
+class PhotoLikes(models.Model):
+    pass
+
+class CommentsLikes(models.Model):
+    pass
+
+class SiteOfDay(models.Model):
+    pass
+
+class SiteOfYear(models.Model):
+    pass
+
+class HonorableMention(models.Model):
+    pass
+
+class MobileOfWeek(models.Model):
+    pass
+
+class DeveloperSite(models.Model):
+    pass
